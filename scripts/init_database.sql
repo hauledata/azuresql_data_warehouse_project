c4e8672b3d0438fe5634bb16f8b586c8de52526a
hauledata <haule.work@gmail.com>
2025-02-13 18:31:28 +0700

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,24 @@
+USE master;
 
+-- Drop and recreate the 'DataWarehouse' database:
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas:
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
